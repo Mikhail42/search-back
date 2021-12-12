79c6bcc1ad5d31b0623d5b6e9d52cee1fbd0c3f9
Mikhail42 <ionkinmikhail@gmail.com>
2021-12-12 11:20:28 +0300

    вернулся к java 11

--- core/src/main/java/org/ionkin/search/model/IntIntPair.java
+++ core/src/main/java/org/ionkin/search/model/IntIntPair.java
@@ -1,4 +1,19 @@
 package org.ionkin.search.model;
 
-public record IntIntPair(int first, int second) {
+public class IntIntPair {
+    private final int first;
+    private final int second;
+
+    public IntIntPair(int first, int second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public int first() {
+        return first;
+    }
+
+    public int second() {
+        return second;
+    }
 }
