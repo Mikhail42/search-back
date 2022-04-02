187b1d6cc0c98b1a45c6ef3feecd95b2ed3ece4e
mika <ionkinmikhail@gmail.com>
2022-04-03 00:59:58 +0300

    fix pageId position creation

--- index/src/main/java/org/ionkin/search/TextArticleIterator.java
+++ index/src/main/java/org/ionkin/search/TextArticleIterator.java
@@ -29,6 +29,7 @@ public class TextArticleIterator {
     static void init() {
         if (wasInit) return;
         try {
+            logger.info("start init TextArticleIterator");
             if (!new File(Util.firstDocidFilenamePath).exists()) {
                 writeFirstDocidFilenameMap();
             }
@@ -41,7 +42,7 @@ public class TextArticleIterator {
             }
             docidPositionMap = new CompactHashMap<>(new IntIntIntTranslator(), Util.docidPosPath);
         } catch (Exception e) {
-            logger.error("Can't read firstdocidFilenameMap", e);
+            logger.error("Can't init TextArticleIterator", e);
         }
     }
 
@@ -64,39 +65,41 @@ public class TextArticleIterator {
         return index;
     }
 
-    public static Iterator<Page> articleTextIterator() {
-        return new Iterator<Page>() {
-            private int firstDocIdIndex = 0;
-            private Iterator<Page> pageIterator;
+    static class PageIterator implements Iterator<Page> {
+        private int firstDocIdIndex = 0;
+        private Iterator<Page> pageIterator;
 
-            {
+        {
+            initPageIterator();
+        }
+
+        private void initPageIterator() {
+            logger.debug("init iterator with index: {}, firstDocId: {}", firstDocIdIndex, firstDocIds[firstDocIdIndex]);
+            String filename = Util.textPath + firstdocidFilenameMap.get(firstDocIds[firstDocIdIndex]);
+            firstDocIdIndex++;
+            try {
+                pageIterator = articleTextIterator(filename);
+            } catch (IOException ioe) {
+                logger.error("Can't read from {}", filename);
+            }
+        }
+
+        @Override
+        public boolean hasNext() {
+            return pageIterator.hasNext() || firstDocIdIndex < firstDocIds.length - 1;
+        }
+
+        @Override
+        public Page next() {
+            if (!pageIterator.hasNext()) {
                 initPageIterator();
             }
+            return pageIterator.next();
+        }
+    }
 
-            private void initPageIterator() {
-                logger.debug("init iterator with index: {}, firstDocId: {}", firstDocIdIndex, firstDocIds[firstDocIdIndex]);
-                String filename = Util.textPath + firstdocidFilenameMap.get(firstDocIds[firstDocIdIndex]);
-                firstDocIdIndex++;
-                try {
-                    pageIterator = articleTextIterator(filename);
-                } catch (IOException ioe) {
-                    logger.error("Can't read from {}", filename);
-                }
-            }
-
-            @Override
-            public boolean hasNext() {
-                return pageIterator.hasNext() || firstDocIdIndex < firstDocIds.length - 1;
-            }
-
-            @Override
-            public Page next() {
-                if (!pageIterator.hasNext()) {
-                    initPageIterator();
-                }
-                return pageIterator.next();
-            }
-        };
+    public static Iterator<Page> articleTextIterator() {
+        return new PageIterator();
     }
 
     public static Iterator<Page> articleTextIterator(String filename) throws IOException {
@@ -107,22 +110,24 @@ public class TextArticleIterator {
     }
 
     static void writeDocIdPositions() throws IOException {
+        logger.info("start read docId positions");
         CompactHashMap<Integer, IntIntPair> docidPositionMap =
                 new CompactHashMap<>(new IntIntIntTranslator());
-        ParallelFor.par((i) -> {
+        for (int i=0; i<firstDocIds.length; i++) {
             int firstDocId = firstDocIds[i];
             String filename = firstdocidFilenameMap.get(firstDocId);
+            logger.debug("read from file {}", Util.textPath + filename);
             WikiParser wikiParser = new WikiParser(Util.textPath + filename);
-            CompactHashMap<Integer, IntIntPair> local = wikiParser.getDocidPositionMap();
-            synchronized (docidPositionMap) {
-                docidPositionMap.putAll(local);
-            }
-        }, 0, firstDocIds.length);
-
+            CompactHashMap<Integer, IntIntPair> docIdToPosition = wikiParser.getDocidPositionMap();
+            docidPositionMap.putAll(docIdToPosition);
+        }
+        logger.info("stop read docId positions. Start write them");
         docidPositionMap.write(Util.docidPosPath);
+        logger.info("stop for docId positions");
     }
 
     static void writeFirstDocidFilenameMap() throws IOException {
+        logger.info("start create map of (first doc id -> filename)");
         Map<Integer, String> firstDocidFilenameMap = new HashMap<>();
         StringBuffer sb = new StringBuffer();
         for (File dir : Util.textDirs()) {
@@ -137,10 +142,12 @@ public class TextArticleIterator {
                 sb.append(dir.getName()).append("/").append(filename).append(", ").append(firstDocId).append("\n");
             }
         }
+        logger.info("stop create map of (first doc id -> filename). Start write it to file");
         FileUtils.write(new File(Util.firstDocidFilenamePath), sb.toString(), StandardCharsets.UTF_8);
     }
 
     static Map<Integer, String> readFirstDocidFilenameMap() throws IOException {
+        logger.info("start read map of (first doc id -> filename)");
         Map<Integer, String> map = new HashMap<>();
         File file = new File(Util.firstDocidFilenamePath);
         String content = FileUtils.readFileToString(file, StandardCharsets.UTF_8);
@@ -151,6 +158,7 @@ public class TextArticleIterator {
             int id = Integer.parseInt(ss[1]);
             map.put(id, filename);
         }
+        logger.info("stop read map of (first doc id -> filename)");
         return map;
     }
 }
