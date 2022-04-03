6e31e09ea041ec111aa14ed7e28b97d923ffac75
mika <ionkinmikhail@gmail.com>
2022-04-03 10:44:00 +0300

    fix parsing wiki pages (fix length of page header)

--- index/src/main/java/org/ionkin/search/WikiParser.java
+++ index/src/main/java/org/ionkin/search/WikiParser.java
@@ -4,6 +4,8 @@ import com.google.common.base.Utf8;
 import org.ionkin.search.model.IntIntPair;
 import org.ionkin.search.map.CompactHashMap;
 import org.ionkin.search.map.IntIntIntTranslator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -12,11 +14,12 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class WikiParser {
+    private static final Logger logger = LoggerFactory.getLogger(WikiParser.class);
     //<doc id="4" url="https://ru.wikipedia.org/wiki?curid=4" title="Базовая статья">
     // Базовая статья
     //</doc>
     private static final String docPatternStr =
-           "<doc id=\"(\\d+)\" .*?(?=title=)title=\"([^\"]+)\">(.*?(?=</doc>))</doc>";
+            "<doc id=\"(\\d+)\" url=\"https://ru\\.wikipedia\\.org/wiki\\?curid=\\d+\" title=\"([^\"]+)\">(.*?(?=</doc>))</doc>";
     private static final Pattern docPattern = Pattern.compile(docPatternStr, Pattern.DOTALL);
 
     private final String fileContent;
@@ -29,29 +32,40 @@ public class WikiParser {
         parseAllPages();
     }
 
+    public ArrayList<Page> getPages() {
+        return pages;
+    }
+
+    public CompactHashMap<Integer, IntIntPair> getDocidPositionMap() {
+        return docidPositionMap;
+    }
+
     public static Page parsePage(String filename, int startPosition, int length) throws IOException {
         String content = new String(IO.read(filename, startPosition, length), StandardCharsets.UTF_8);
-        Matcher m = docPattern.matcher(content);
-        if (m.find()) {
-            int id = Integer.parseInt(m.group(1));
-            String title = m.group(2);
-            String text = m.group(3);
+        Matcher docMatcher = docPattern.matcher(content);
+        if (docMatcher.find()) {
+            int id = Integer.parseInt(docMatcher.group(1));
+            String title = docMatcher.group(2);
+            String text = docMatcher.group(3);
             return new Page(id, title, text);
         } else {
+            logger.warn("Can't parse page in {}. position (in bytes): {}, length: {}", filename, startPosition, length);
             return null;
         }
     }
 
     private void parseAllPages() {
-        Matcher m = docPattern.matcher(fileContent);
+        Matcher docMatcher = docPattern.matcher(fileContent);
         int currentByteLength = 0;
-        while (m.find()) {
-            int id = Integer.parseInt(m.group(1));
-            String title = m.group(2);
-            String text = m.group(3);
+        while (docMatcher.find()) {
+            int id = Integer.parseInt(docMatcher.group(1));
+            String title = docMatcher.group(2);
+            String text = docMatcher.group(3);
             pages.add(new Page(id, title, text));
 
-            int head = Utf8.encodedLength("<doc id=\"" + id + "\" title=\"" + title + "\">");
+            String header = "<doc id=\"" + id + "\" url=\"https://ru.wikipedia.org/wiki?curid=" + id
+                    + "\" title=\"" + title + "\">";
+            int head = Utf8.encodedLength(header);
             int tail = Utf8.encodedLength("</doc>\n");
 
             int start = currentByteLength;
@@ -60,13 +74,5 @@ public class WikiParser {
 
             docidPositionMap.put(id, new IntIntPair(start, length));
         }
-    }
-
-    public ArrayList<Page> getPages() {
-        return pages;
-    }
-
-    public CompactHashMap<Integer, IntIntPair> getDocidPositionMap() {
-        return docidPositionMap;
     }
 }
