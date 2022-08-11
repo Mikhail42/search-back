ebde74f5ef325ad9b9841e2f261e8d06f305ce20
mika <ionkinmikhail@gmail.com>
2022-08-12 01:39:43 +0300

    add new fields to Revision

--- xml-parser/src/main/java/org/ionkin/search/xmlparser/model/Revision.java
+++ xml-parser/src/main/java/org/ionkin/search/xmlparser/model/Revision.java
@@ -6,8 +6,41 @@ import javax.xml.bind.annotation.XmlElement;
 
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Revision {
+    @XmlElement(name="id")
+    private Integer id;
+    @XmlElement(name="parentId")
+    private Integer parentId;
+    @XmlElement(name="timestamp")
+    private String timestamp;
     @XmlElement(name="text")
     private String text;
+
+    public Revision() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(Integer parentId) {
+        this.parentId = parentId;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
 
     public String getText() {
         return text;
